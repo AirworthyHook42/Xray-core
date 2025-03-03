2074d5ce3858f2fa28d2d7b5aa7604ce4fe458b1
RPRX <63339210+RPRX@users.noreply.github.com>
2025-03-03 14:09:59 +0000

    DNS DoH: Use Chrome's fingerprint & keepAlivePeriod
    https://github.com/XTLS/Xray-core/discussions/4430#discussioncomment-12374292

--- common/crypto/crypto.go
+++ common/crypto/crypto.go
@@ -1,2 +1,15 @@
 // Package crypto provides common crypto libraries for Xray.
 package crypto // import "github.com/xtls/xray-core/common/crypto"
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func RandBetween(left int64, right int64) int64 {
+	if left == right {
+		return left
+	}
+	bigInt, _ := rand.Int(rand.Reader, big.NewInt(right-left))
+	return left + bigInt.Int64()
+}
